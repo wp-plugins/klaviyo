cfbb6f4e845bc76b7c716c98f8d712a39f7b0a97
bialecki <bialecki@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-01 22:59:48 +0000

    Adding in unknown files.
    git-svn-id: https://plugins.svn.wordpress.org/klaviyo/trunk@849600 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ inc/kla-analytics.php
@@ -0,0 +1,50 @@
+<?php
+class WPKlaviyoAnalytics {
+
+    function __construct() {
+        add_action('wp_footer', array(&$this, 'insert_analytics'));
+    }
+
+    function insert_analytics() {
+        global $current_user;
+        wp_reset_query();
+
+        get_currentuserinfo();
+        $klaviyo_settings = get_option('klaviyo_settings');
+
+        if ($klaviyo_settings['public_api_key'] == '') {
+          return;
+        }
+
+        echo "\n" . '<!-- Start Klaviyo // Plugin Version: ' . KLAVIYO_PLUGIN_VERSION .' -->' . "\n";
+        echo '<script type=\"text/javascript\">' . "\n";
+        echo 'var _learnq = _learnq || [];' . "\n";
+
+        echo "_learnq.push([\"account\", \"" . $klaviyo_settings['public_api_key'] . "\"]);\n";
+
+        if ($current_user->user_email) {
+            echo '_learnq.push(["identify", {' . "\n";
+            echo '  $email : "' . $current_user->user_email . '"' . "\n";
+            echo '}]);' . "\n";
+        } else {
+            // See if current user is a commenter
+            $commenter = wp_get_current_commenter();
+            if ($commenter['comment_author_email']) {
+                echo '_learnq.push(["identify", {' . "\n";
+                echo '  $email : "' . $commenter['comment_author_email'] . '"' . "\n";
+                echo '}]);' . "\n";
+            }
+        } 
+
+        echo '(function() {' . "\n";
+        echo '   var pa = document.createElement("script"); pa.type = "text/javascript"; pa.async = true;' . "\n";
+        echo '   pa.src = "//a.local-klaviyo.com/media/js/analytics/analytics.js";' . "\n";
+        echo '   var s = document.getElementsByTagName("script")[0];' . "\n";
+        echo '   s.parentNode.insertBefore(pa, s);' . "\n";
+        echo '})();' . "\n";
+        echo '</script>' . "\n";
+        echo '<!-- end: Klaviyo Code. -->' . "\n";
+    }
+}
+
+?>
